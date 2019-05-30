232ac4be438cbbe7a4bd4544abec5955846b5539
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-05-30 15:31:13 -0300

    add primary arquives
    adicionando os arquivos primários

---
+++ Jogo_Encanamento/src/jogo_encanamento/Jogo_Encanamento.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogo_encanamento;
+
+/**
+ *
+ * @author Douglas Dias
+ */
+public class Jogo_Encanamento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
